5132d0b155ea13edc038bd8005d8862fcd517c03
AnDNTSE184673 <andntse184673@fpt.edu.vn>
2024-06-28 15:41:33 +0700

    Delete swp directory

--- swp/src/main/java/com/swp/repository/MaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.swp.entity.Material;
-
-public interface MaterialRepository extends JpaRepository<Material,String>{
-
-}
--- swp/src/main/java/com/swp/repository/ShellRepository.java
+++
@@ -1,8 +0,0 @@
-package com.swp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.swp.entity.Shell;
-public interface ShellRepository extends JpaRepository<Shell, String> {
-
-}
